bbc840c7204ec27e87831f1a5e9458ef94b861f4
david fontaine <devilangel6089@gmail.comgit config --global user.name AngelDevil96>
2024-04-09 13:14:45 +0200

    1e save

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+const question = [
+  new Question(
+    "Quelle méthode Javascript permet de filtrer les éléments d'un tableau",
+    ["indexOf()", "map()", "filter()", "reduce()"],
+    "filter()"
+  ),
+  new Question(
+    "Quelle méthode Javascript permet de vérifier si un élément figure dans un tableau",
+    ["isNaN()", "includes()", "findIndex()", "isOdd()"],
+    "includes()"
+  ),
+  new Question(
+    "Quelle méthode transforme du JSON en un objet Javascript",
+    ["JSON.parse()", "JSON.stringify()", "JSON.object()", "JSON.toJS()"],
+    "JSON.parse()"
+  ),
+  new Question(
+    "Quelle objet Javascript permet d'arrondir à l'entier le plus proche",
+    ["Math.ceil()", "Math.floor()", "Math.round()", "Math.random()"],
+    "Math.round()"
+  ),
+];
+
+// Quiz display
+// Affichage choix + prise en compte du choix
+// Game Logic
+// Create Quiz
